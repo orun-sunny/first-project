b0e438d605710756c3a7bf0d208ddd99ebc03ae1
orun-sunny <sunny.mukut@gmail.com>
2025-01-01 18:59:53 +0600

    working on unversity management

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,32 @@
-import express, { Application, Request, Response } from 'express';
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import cors from 'cors';
-import { StudentRoutes } from './modules/student/student.route';
+import express, { Application, Request, Response } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorhandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
+
 const app: Application = express();
 
-//parser
+//parsers
 app.use(express.json());
 app.use(cors());
 
+// application routes
+app.use('/api/v1', router);
 
+const test = (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+};
 
-app.use('/api/v1/students',StudentRoutes)
+app.get('/', test);
 
-const getAController = (req: Request, res: Response) => {
-    const a = 10;
-    res.send(a);
-}
+app.use(globalErrorHandler);
 
-app.get('/', getAController);
+//Not Found
+app.use(notFound);
+
 export default app;
